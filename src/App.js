15cdf4d4b85453ebf462d267d9af7195f0e722a9
awanhitam78 <85306473+awanhitam78@users.noreply.github.com>
2022-03-08 12:02:03 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
 // Constants
+const NINJA_LINK = `https://testnets.opensea.io/assets/mumbai/0x7dbbac81d024f3a31622a584064f3f6d4e68c47b/0`;
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
@@ -15,7 +16,11 @@ const App = () => {
 				<div className="header-container">
 					<header>
             <div className="left">
-              <p className="title">🐱‍👤 Ninja Name Service</p>
+              <p className="title">🐱‍👤 <a 
+href={NINJA_LINK}
+target="_blank"
+rel="noreferrer">
+awanhitam.ninja</a> 👈😁👌</p>
               <p className="subtitle">Your immortal API on the blockchain!</p>
             </div>
 					</header>
